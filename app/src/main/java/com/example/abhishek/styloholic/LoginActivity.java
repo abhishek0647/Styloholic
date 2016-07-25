1bc972532c75958f6a5267b9b5395419998fb0ee
Abhishek Kumar <abhishek0647@gmail.com>
2016-07-26 00:36:57 +0530

    Resolved dependency issues for google signin

--- app/src/main/java/com/example/abhishek/styloholic/LoginActivity.java
+++ app/src/main/java/com/example/abhishek/styloholic/LoginActivity.java
@@ -1,13 +1,117 @@
 package com.example.abhishek.styloholic;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+//    private SignInButton mSignInButton;
+    private Button mSignOutButton;
+    private Button mRevokeButton;
+    private TextView mStatus;
+    private TextView mStatusTextView;
+
+    private static final int RC_SIGN_IN = 9001;
+
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+//        mSignInButton = (SignInButton) findViewById(R.id.sign_in_button);
+//        mSignOutButton = (Button) findViewById(R.id.sign_out_button);
+//        mRevokeButton = (Button) findViewById(R.id.revoke_access_button);
+//        mStatus = (TextView) findViewById(R.id.sign_in_status);
+//
+//        findViewById(R.id.sign_in_button).setOnClickListener(this);
+//        mSignOutButton.setOnClickListener(this);
+//        mRevokeButton.setOnClickListener(this);
+//
+//        mGoogleApiClient = buildApiClient();
+
+        // Views
+        mStatusTextView = (TextView) findViewById(R.id.status);
+
+        // Button listeners
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+        findViewById(R.id.sign_out_button).setOnClickListener(this);
+        findViewById(R.id.disconnect_button).setOnClickListener(this);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // [START build_client]
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+//                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+        // [END build_client]
+
+    }
+
+//    GoogleApiClient buildApiClient() {
+//        return new GoogleApiClient.Builder(this)
+//                .addConnectionCallbacks(this)
+//                .addOnConnectionFailedListener(this)
+//                .addApi(Plus.API, Plus.PlusOptions.builder().build())
+//                .addScope(new Scope(Scopes.PROFILE))
+//                .build();
+//    }
+
+    // [START signIn]
+    private void signIn() {
+//        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+//        startActivityForResult(signInIntent, RC_SIGN_IN);
+        Intent intent = new Intent(LoginActivity.this, SplashActivity.class);
+        startActivity(intent);
+    }
+    // [END signIn]
+
+    private void updateUI(boolean signedIn) {
+        if (signedIn) {
+            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+//            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
+        } else {
+//            mStatusTextView.setText(R.string.signed_out);
+
+            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+//            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
+        }
+    }
+
+    // [START signOut]
+    private void signOut() {
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient);
+
+    }
+    // [END signOut]
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+            case R.id.sign_out_button:
+                signOut();
+                break;
+//            case R.id.disconnect_button:
+//                revokeAccess();
+//                break;
+        }
     }
 }
